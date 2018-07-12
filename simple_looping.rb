e95842da92e5c8828b2999dfde79e1972033d2c9
Michelle Kim <pasolre95kim@gmail.com>
2018-07-12 01:14:55 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -7,7 +7,7 @@ def loop_iterator(number_of_times)
        puts phrase
        count+= 1
        break if count>= 7
-   end   
+   end
  end
   # Code your solution here using the "loop" keyword to puts out the below phrase
   # This and all the other methods take an argument of an integer
